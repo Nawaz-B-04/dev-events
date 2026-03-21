0c731ccc81518a53ec5ea094c55564a4dda467f9
Nawaz B <nawazishb2004@gmail.com>
2026-03-22 00:29:12 +0530

    feat: implement complete application

--- proxy.ts
+++ proxy.ts
@@ -1,4 +1,3 @@
-
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { jwtVerify } from "jose";
@@ -10,13 +9,10 @@ export async function middleware(request: NextRequest) {
         request.nextUrl.pathname.startsWith("/my-events");
 
     if (isProtected) {
-
         const token = request.cookies.get("auth_token")?.value;
-
         if (!token) {
             return NextResponse.redirect(new URL("/login", request.url));
         }
-
         try {
             const secretKey = new TextEncoder().encode(process.env.JWT_SECRET || "fallback_dev_password");
             await jwtVerify(token, secretKey);
@@ -25,7 +21,6 @@ export async function middleware(request: NextRequest) {
             return NextResponse.redirect(new URL("/login", request.url));
         }
     }
-
     return NextResponse.next();
 }
 
